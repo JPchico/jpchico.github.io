03a9b1172f53ca56febd0d357dde79c1bd3cbc9b
Jonathan Chico <jonathanpchico@gmail.com>
2024-04-22 22:12:19 +0200

    Fixing the css in several components so that it works for mobiles.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'jpchico.github.io' title`, () => {
+  it(`should have the 'Jonathan Chico' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('jpchico.github.io');
+    expect(app.title).toEqual('Jonathan Chico');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, jpchico.github.io');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Jonathan Chico');
   });
 });
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,31 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { NavbarData } from '../../data_model/navbar-data';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule,MatButtonModule, RouterModule],
+  imports: [MatToolbarModule,MatButtonModule, RouterModule, MatMenuModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent implements OnInit {
 
-  menuData: NavbarData[] = []
+  menuData: NavbarData[] = [
+    {
+      title: 'Projects',
+      route: 'projects'
+    },
+    {
+      title: 'Publications',
+      route: 'publications'
+    }
+  ]
 
-  ngOnInit(): void{
-    this.menuData = [
-      {
-        title: 'Projects',
-        route: 'projects'
-      },
-      {
-        title: 'Publications',
-        route: 'publications'
+  isPortable: boolean = false;
+
+  constructor(private responsive: BreakpointObserver){}
+
+
+  ngOnInit(): void {
+    this.responsive.observe(Breakpoints.HandsetPortrait).subscribe(result => {
+
+      this.isPortable = false;
+
+      if (result.matches) {
+        this.isPortable = true;
       }
-    ]
+
+    });
   }
 
 }
